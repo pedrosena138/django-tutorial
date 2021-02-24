fc0c283b64719d0ab344af0ff250105789ac59e7
Pedro Sena <pmfsl@cin.ufpe.br>
2021-02-24 11:41:09 -0300

    update votes and results views, create vote form

--- djangoApp/polls/views.py
+++ djangoApp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -26,9 +27,39 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking ate the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        error = {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        }
+
+        return render(request, 'polls/detail.html', error)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prev ents data from being posted twice if a
+        # user hits the Back button.
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# The code for our vote() view does have a small problem.
+# It first gets the selected_choice object from the database, then computes the new value of votes, and then saves it back to the database.
+# If two users of your website try to vote at exactly the same time, this might go wrong: The same value, let’s say 42, will be retrieved for votes.
+# Then, for both users the new value of 43 is computed and saved, but 44 would be the expected value.
+
+# This is called a race condition. If you are interested, you can read Avoiding race conditions using F() to learn how you can solve this issue.
